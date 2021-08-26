21850dd9e2d8a8fe1a0aa11cb717e24ad5973b57
moshabbir-dotcom <moshabbir@ymail.com>
2021-08-26 12:29:52 +0000

    add js code for updating scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -54,12 +56,20 @@ function calculateCorrectAnswer() {
 
 }
 
+/**Gets current score from DOM and increments it by 1 */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
+/**Get talley of incorrect answers from DOM and increments by 1 */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
